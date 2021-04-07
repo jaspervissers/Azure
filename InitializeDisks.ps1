cc511559f7df076d1f5e66de5ceb1d7314b7da9a
jaspervissers <81812431+jaspervissers@users.noreply.github.com>
2021-04-07 09:12:18 +0200

    Update InitializeDisks.ps1

--- InitializeDisks.ps1
+++ InitializeDisks.ps1
@@ -2,7 +2,7 @@ $disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
 
 $letters = 70..89 | ForEach-Object { [char]$_ }
 $count = 0
-$labels = "data1","data2"
+$labels = "Index","Log","Apps","Index-backup"
 
 foreach ($disk in $disks) {
    $driveLetter = $letters[$count].ToString()
